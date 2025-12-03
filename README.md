76b9e7f9c51d76e7f91db897dfab3f9626f03d56
Richard <94.nagy.richard@gmail.com>
2025-12-03 13:41:08 +0100

    1.0 Hello Word app

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hello World React App
+
+Egyszerű React alkalmazás, amely HTTP-n (http://localhost:8080) keresztül elérhető, és kiírja: **"Hello, World!"**
+
+## Követelmények
+
+- Node.js
+- npm
+- Docker (Docker Desktop)
+- Git
+
+## Telepítés
+
+```bash
+npm install
---
+++ src/app.js
@@ -0,0 +1,13 @@
+
+  import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial" }}>
+      <h1>Hello, World!</h1>
+      <p>Ez egy egyszerű React alkalmazás, ami HTTP-n elérhető a localhost:8080 címen.</p>
+    </div>
+  );
+}
+
+export default App;
